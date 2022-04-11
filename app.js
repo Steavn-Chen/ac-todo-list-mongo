a172b51b9674d62d08b8871a4ab6efb96fb3b12a
Steavn Chen <tom9876555@yahoo.com.tw>
2022-04-12 03:45:10 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
-// const mongoose = require('mongoose')
 const exshbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 require('./config/mongoose.js')
 
@@ -16,6 +16,11 @@ app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(router)
 
