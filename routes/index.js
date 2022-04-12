cf0c598115cf37d1fa7c5199340c33a26fef41bd
Steavn Chen <tom9876555@yahoo.com.tw>
2022-04-12 14:10:41 +0800

    feat: user authentication in todo and home routers

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const home = require('./modules/home.js')
 const todos = require('./modules/todos.js')
 const users = require('./modules/users.js')
 
-router.use('/', home)
-router.use('/todos', todos)
+const { authenticated } = require('../middleware/auth.js')
+
+router.use('/todos', authenticated, todos)
 router.use('/users', users)
+router.use('/', authenticated, home)
 
 module.exports = router
