5eb8a9ac19e155bb1ddb01a875e0bea6d83310f9
Steavn Chen <tom9876555@yahoo.com.tw>
2022-04-12 15:33:01 +0800

    feat: add relations between User and Todo models

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,8 @@ const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/todo-list-g'
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+  useCreateIndex: true,
+})
 
 const db = mongoose.connection;
 db.on("error", () => {
--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -8,33 +8,37 @@ router.get("/new", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  const data = String(req.body.name).split(',').map( i => ({ name: i}))
-  return Todo.insertMany(data )
+  const userId = req.user._id 
+  const data = String(req.body.name).split(',').map( i => ({ name: i, userId: userId}))
+  return Todo.insertMany(data)
     .then(() => res.redirect("/"))
     .catch((error) => console.error(error));
 });
 
 router.get("/:todo_id", (req, res) => {
-  const id = req.params.todo_id;
-  return Todo.findById(id)
+  const _id = req.params.todo_id
+  const userId = req.user._id
+  return Todo.findOne({ _id, userId })
     .lean()
-    .then((todo) => res.render("detail", { todo }))
-    .catch((error) => console.error(error));
-});
+    .then((todo) => res.render('detail', { todo }))
+    .catch((error) => console.error(error))
+})
 
 router.get("/:todo_id/edit", (req, res) => {
-  const id = req.params.todo_id
-  return Todo.findById(id)
+  const _id = req.params.todo_id
+  const userId = req.user._id
+  return Todo.findOne({ _id, userId })
     .lean()
-    .then((todo) => res.render("edit", { todo }))
-    .catch((error) => console.error(error));
+    .then((todo) => res.render('edit', { todo }))
+    .catch((error) => console.error(error))
 });
 
 router.put("/:todo_id", (req, res) => {
-  const id = req.params.todo_id;
-  const { name, isDone } = req.body;
+  const id = req.params.todo_id
+  const userId = req.user._id
+  const { name, isDone } = req.body
   return Todo.updateOne(
-    { _id: id },
+    { _id: id, userId: userId },
     { $set: { name, isDone: isDone === "on" } }
   )
     .then(() => res.redirect(`/todos/${id}/edit`))
@@ -51,16 +55,18 @@ router.put("/:todo_id", (req, res) => {
 });
 
 router.delete("/:todo_id", (req, res) => {
-  const _id = req.params.todo_id;
+  
   // return Todo.deleteOne({_id: _id})
   //   .then(() => res.redirect('/'))
   //   .catch(err => console.log(err))
-  return Todo.findById(_id)
+  const _id = req.params.todo_id
+  const userId = req.user._id
+  return Todo.findOne({ _id, userId })
     .then((todo) => {
-      todo.remove();
+      todo.remove()
     })
-    .then(() => res.redirect("/"))
-    .catch((err) => console.error(err));
+    .then(() => res.redirect('/'))
+    .catch((err) => console.error(err))
 });
 
 
