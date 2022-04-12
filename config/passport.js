f6ca494807d6b96a244d92026802b0a14065b3f3
Steavn Chen <tom9876555@yahoo.com.tw>
2022-04-12 19:48:54 +0800

    feat: add bcrypt to login route

--- config/passport.js
+++ config/passport.js
@@ -18,10 +18,16 @@ module.exports = app => {
       if (!user) {
         return done(null, false, { message: '這個 Email 還未被註冊 !'})
       }
-      if (user.password !== password) {
-        return done(null, false, { message: 'Email 或 Password 錯誤.'})
-      }
-      return done(null, user)
+      console.log(user)
+      return bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          // if (user.password !== password) {
+          if (!isMatch) {
+            return done(null, false, { message: 'Email 或 Password 錯誤.' })
+          }
+          return done(null, user)
+        })
+        .catch(err => console.log(err))
     })
     .catch((err) => done(err, false))
   } 
@@ -37,3 +43,48 @@ module.exports = app => {
       .catch(err => done(err, null))
   }) 
 }
+
+
+
+// const passport = require('passport')
+// const bcrypt = require('bcryptjs')
+// const LocalStrategy = require('passport-local').Strategy
+// const User = require('../models/user.js')
+
+// module.exports = (app) => {
+//   app.use(passport.initialize())
+//   app.use(passport.session())
+//   passport.use(
+//     new LocalStrategy(
+//       {
+//         usernameField: 'email',
+//         passwordField: 'password',
+//         passReqToCallback: true,
+//       },
+//       (req, email, password, done) => {
+//         // console.log(email , password)
+//         User.findOne({ email: email })
+//           .then((user) => {
+//             if (!user) {
+//               return done(null, false, { message: '這個 Email 還未被註冊 !' })
+//             }
+//             if (user.password !== password) {
+//               return done(null, false, { message: 'Email 或 Password 錯誤.' })
+//             }
+//             return done(null, user)
+//           })
+//           .catch((err) => done(err, false))
+//       }
+//     )
+//   )
+//   passport.serializeUser((user, done) => {
+//     done(null, user.id)
+//   })
+
+//   passport.deserializeUser((id, done) => {
+//     User.findById(id)
+//       .lean()
+//       .then((user) => done(null, user))
+//       .catch((err) => done(err, null))
+//   })
+// }
